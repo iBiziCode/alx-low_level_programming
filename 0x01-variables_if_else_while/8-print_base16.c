181baabc08c59583eb980a09d983ef671c8f1a70
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-02-18 01:19:28 +0200

    added hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main -  entry point
+*Return: always 0 for success
+*/
+int main(void)
+{
+	int n  = 0;
+
+	char c = 'a';
+
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}

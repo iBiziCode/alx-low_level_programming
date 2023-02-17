d110207bf33de7b2dc836b31fb4852053fca23ef
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-02-18 01:07:07 +0200

    added print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main -  entry point
+*Return: always 0 for success
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

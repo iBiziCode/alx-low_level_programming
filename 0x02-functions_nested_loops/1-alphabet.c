083cd753832d533e37fff66cf8a5d31dfd4497b9
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-02-28 20:40:32 +0200

    added lowcase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - Entry point
+* Return: always 0 for success
+*/
+
+void print_alphabets(void)
+{
+	char lowcase;
+
+	for(lowcase='a'; lowcase <= 'z'; lowcase++)
+		_putchar(lowercase);
+	
+	_putchar('\n');
+}

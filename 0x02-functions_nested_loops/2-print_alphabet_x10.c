2bc6cf6a13beeeac6ef327c1b275b5a105375761
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-02-28 20:54:16 +0200

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,9 @@
 void print_alphabet_x10(void)
 {
 	char lowcase;
+	int i;
 
-	for (int i = 0; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 		for (lowcase = 'a'; lowcase <= 'z'; lowcase++)
 			_putchar(lowcase);

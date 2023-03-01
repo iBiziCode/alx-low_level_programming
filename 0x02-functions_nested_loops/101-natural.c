8c4f5951d076d6769672ecf891851dc1d464e069
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-03-01 13:37:41 +0200

    added natural number

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,51 +1,19 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* print_times_table - Prints @n times table
-* @n: the number of times table
-* Return: none
+* main - Prints sum of multiples of 3 and 5
+* Return: 0 for success
 */
 
-void print_times_table(int n)
+int main(void)
 {
-	int num, mult, prod;
+	int total = 0, num;
 
-	if (n > 15 || n < 0)
-		return;
-
-	for (num = 0; num <= n; num++)
+	for (num = 0; num < 1024; num++)
 	{
-		for (mult = 1; mult < 10; mult++)
-		{
-			prod = num * mult;
-
-			if (prod < 99)
-			{
-				_putchar(prod / 100 + '0');
-				_putchar(prod / 10 + '0');
-				_putchar(prod % 10 + '0');
-			}
-			else if (prod > 9)
-			{
-				_putchar(' ');
-				_putchar(prod / 10 + '0');
-				_putchar(prod % 10 + '0');
-			}
-			else if (y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(prod + '0');
-			}
-			else
-				_putchar(prod + '0');
-
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-		_putchar('\n');
+		if (num % 3 == 0)
+			total += num;
+		else if (num % 5 == 0)
+			total += num;
 	}
 }

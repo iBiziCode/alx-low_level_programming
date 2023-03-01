09aba3646a2e7f410ee452cc8db9e9df5e6b384e
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-03-01 07:01:50 +0200

    added sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_sign - prints the sign of an integer
+* @n: the integer
+* Return: 0 for success
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

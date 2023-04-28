1751d14ae8fd92f5e7da3ff810d8b385abd70f6a
Bizimana Ibrahim <bizimanaibrahim0@gmail.com>
2023-04-28 05:58:29 +0200

    fix add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -23,6 +23,7 @@ int _strlen(char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int count = 0;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -33,7 +34,9 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = _strlen(str);
+	while (str[count])
+		count++;
+	new_node->len = count;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
